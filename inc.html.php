cad1c5afc78415cb604ccf06846bbf33ea24ede4
AyrA <github@bitm.sg>
2021-03-31 00:52:10 +0200

    Add setting for unsafe algorithms

--- inc.html.php
+++ inc.html.php
@@ -239,6 +239,7 @@
 			$config['stdoutonly']=av($_POST,'stdoutonly')==='1';
 			$config['fastmedia']=av($_POST,'fastmedia')==='1';
 			$config['ini-strip-comments']=av($_POST,'ini-strip-comments')==='1';
+			$config['unsafe-crypto']=av($_POST,'unsafe-crypto')==='1';
 			setConfig($config);
 			exit(redir(selfurl() . '?action=settings&msg=Settings+changed'));
 		}
@@ -312,9 +313,11 @@
 					<input type="submit" value="Change" />
 				</form>
 				<small>Changing the password will immediately log out all other sessions</small>
-				<h2>Performance enhancements</h2>
+				<h2>Settings</h2>
 				<form method="post" action="' . he(selfurl()) . '?action=settings">
 					<input type="hidden" name="mode" value="performance" />
+					<label><input type="checkbox" name="unsafe-crypto" value="1" ' . (av($config,'unsafe-crypto')?'checked':'') . ' /> Show unsafe cryptographic algorithms</label><br />
+					<small>Shows unsafe algorithms in the file encryption utility</small><br />
 					<label><input type="checkbox" name="ini-strip-comments" value="1" ' . (av($config,'ini-strip-comments')?'checked':'') . ' /> Strip comments in PHP ini</label><br />
 					<small>Removes comments and empty lines from the PHP ini file in the info viewer</small><br />
 					<label><input type="checkbox" name="fastmedia" value="1" ' . (av($config,'fastmedia')?'checked':'') . ' /> Detect file type by file extension only</label><br />
