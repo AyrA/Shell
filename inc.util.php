9d352e5a1e8b3744be1a3be40efc18f9bf5a482c
AyrA <github@bitm.sg>
2021-04-17 14:24:26 +0200

    Update Version

--- inc.util.php
+++ inc.util.php
@@ -4,7 +4,7 @@
 	*/
 
 	//Shell version
-	define('SHELL_VERSION','1.1.0');
+	define('SHELL_VERSION','1.1.1');
 	//Date format for HTTP headers
 	define('HTTP_DATE','D, d M Y H:i:s T');
 	//The date format for whenever a date is being displayed
